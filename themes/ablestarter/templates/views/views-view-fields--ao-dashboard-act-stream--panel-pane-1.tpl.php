5760e8e5733ab802b6e12a26bf9ac3b6055f6612
Michael Haggerty <mhaggerty@trellon.com>
2013-12-12 04:29:17 -0500

    by techsoldaten: commiting backend theme for hopefully the last time

--- themes/ablestarter/templates/views/views-view-fields--ao-dashboard-act-stream--panel-pane-1.tpl.php
+++ themes/ablestarter/templates/views/views-view-fields--ao-dashboard-act-stream--panel-pane-1.tpl.php
@@ -24,36 +24,32 @@
  * @ingroup views_templates
  */
 	
-	// need to build strings around the different types	
-	$contact_name = $fields['contact_name']->content;
-	
-	$class_name = '';
-	$activity_string = '';
-	
-	if($fields['type']->raw == 'cmcd_donation'){
-		$class_name = 'ao_activity_donation';
-		$activity_string = $contact_name . ' made a donation to ' . $fields['field_cmcd_page']->content;
-	} else 
-	if($fields['type']->raw == 'cmcp_petition_signature'){
-		$class_name = 'ao_activity_petition';
-		$activity_string = $contact_name . ' signed the petition ' . $fields['field_cmcp_petition_page']->content;
-	} else 
-	if($fields['type']->raw == 'cmcev_event_registration'){
-		$class_name = 'ao_activity_event';
-		$activity_string = $contact_name . ' registered for the event ' . $fields['field_cmcev_event']->content;
-	} else 
-	if($fields['type']->raw == 'cmcv_volunteer_commitment'){
-		$class_name = 'ao_activity_volunteer';
-		$activity_string = $contact_name . ' volunteered for ' . $fields['field_cmcv_vo_reference']->content;
-	}
-	
+  global $check;
+  
+  if($check !== 1){
+    // dpm($fields);
+    $check = 1;
+  }
+
+
+
 ?>
-<div class="ao_activity_feed_item">
-	<div class="ao_activity_feed_icon <?php print $class_name; ?>"></div>
-	<div class="ao_activity_feed_string">
-		<?php print $activity_string; ?>
-	</div>
-	<div class="ao_activity_feed_date">
-		<?php print $fields['field_activity_date']->content; ?>
-	</div>
+<div class="crm-core-activity <?php print $fields['type']->raw; ?>">
+  <div class="crm-core-activity-icon"><span></span></div>
+  <div class="crm-core-activity-info">
+      
+    <div class="crm-core-activity-actions">
+      <?php print $fields['nothing_4']->content; ?>
+    </div>
+    
+    <?php print $fields['contact_name']->content; ?>
+    
+    <?php print $fields['activity_string']->content; ?>
+    
+    <?php print $fields['nothing']->content; ?>
+    
+    <div class="crm-core-activity-date">
+      <?php print $fields['nothing_1']->content; ?>
+    </div>
+  </div>
 </div>
