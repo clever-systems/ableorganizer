828e817216b74b78151a951bb3bf8b9b6f37e75f
Michael Haggerty <mhaggerty@trellon.com>
2014-03-01 17:47:09 -0500

    by techsoldaten: adding pop ups to charts

--- themes/ao_contact_management/js/script.js
+++ themes/ao_contact_management/js/script.js
@@ -114,6 +114,103 @@ Drupal.behaviors.ao_contact_management = {
             });
           }
         }
+        
+        // show tooltips on flot charts
+        // check for anything flot
+        var charts = $('.flot');
+        if(charts.length > 0){
+          
+          var previousPoint = null, pageX, pageY, followMouse, flotTooltipWidth, flotTooltipHeight, tooltip;
+          
+          $(document).bind('mousemove',function(e){ 
+            
+            pageX = event.pageX;
+            pageY = event.pageY;
+            
+            if(tooltip && followMouse == true){
+              tooltip.css('left',pageX-flotTooltipWidth/2);
+              tooltip.css('top',pageY-flotTooltipHeight-30);
+            }
+            
+          }); 
+          
+          
+          $(".flot").bind("plothover", function(event, pos, item){
+            
+            // make sure we are over a point
+            if (item) {
+              
+              document.body.style.cursor = 'pointer';
+              
+              if(item.dataIndex !== previousPoint){
+                // remove any existing tooltips
+                $("#flot-tooltip").remove();
+              }
+              
+              if (previousPoint != item.dataIndex) {
+                
+                var usex, usey, label = '', prefix = '', suffix = '', content = 0;
+                
+                if(item.pageX){
+                  usex = item.pageX;
+                  usey = item.pageY-10;
+                  followMouse = false;
+                } else {
+                  usex = pageX;
+                  usey = pageY;
+                  followMouse = true;
+                }
+                
+                // set the current item to the tooltip
+                previousPoint = item.dataIndex;
+                
+                // remove any existing tooltips
+                $("#flot-tooltip").remove();
+                
+                // set the content
+                if(item.series.useLabel == 1){
+                  label = item.series.label;
+                }
+                if(item.series.prefix){
+                  prefix = item.series.prefix;
+                }
+                if(item.series.suffix){
+                  suffix = item.series.suffix;
+                }
+                if(item.series.data[item.dataIndex]){
+                  content = label + prefix + item.series.data[item.dataIndex][1] + suffix;
+                }
+                
+                // show the tooltip
+                if(content !== ''){
+                  showTooltip(usex, usey, content);
+                }
+                
+              }
+              
+            } else {
+              document.body.style.cursor = 'default';
+              $("#flot-tooltip").remove();
+              previousPoint = null;
+            }
+          });
+          
+        }
+        
+        // display a tooltip
+        function showTooltip(x, y, contents) {
+          $('<div id="flot-tooltip">' + contents + '</div>').css( {
+              top: y,
+              left: x,
+          }).appendTo("body").fadeIn(5000);
+          
+          tooltip = $('#flot-tooltip');
+          flotTooltipHeight = tooltip.height();
+          flotTooltipWidth = tooltip.width();
+          tooltip.css('top', y - flotTooltipHeight-30);
+          tooltip.css('left', x - flotTooltipWidth/2);
+        }
+        
 
         // detect page reloads
         $(window).resize(function() {
