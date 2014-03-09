3f7d30827a2f08179b7c43a30259a05f8a8d0a19
Michael Haggerty <mhaggerty@trellon.com>
2014-03-09 09:06:43 -0700

    by techsoldaten: adding theme for new contact types

--- themes/ao_contact_management/js/script.js
+++ themes/ao_contact_management/js/script.js
@@ -1,19 +1,8 @@
 /**
  * @file
- * A JavaScript file for the theme.
- *
- * In order for this JavaScript to be loaded on pages, see the instructions in
- * the README.txt next to this file.
+ * Basic UI handling for contact management.
  */
-
-// JavaScript should be made compatible with libraries other than jQuery by
-// wrapping it with an "anonymous closure". See:
-// - https://drupal.org/node/1446420
-// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
-
-
-// To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.ao_contact_management = {
     attach: function (context) {
       // handles display of meny navigation
@@ -78,7 +67,7 @@ Drupal.behaviors.ao_contact_management = {
             
             // add the button for VBO to the form
             var new_btn = '<div class="btn-vbo btn-disabled">With Selected<div class="btn-expandable"><ul class="links">' + menu_opts + '</ul></div></div>';
-            var other_btns = $('.view-header').find('.detail-buttons');
+            var other_btns = $('.view-header').children();
             $('.view-header').prepend(new_btn);
             
             if(other_btns.length > 0){
